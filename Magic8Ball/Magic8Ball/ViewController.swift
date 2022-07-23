36129e6d0297f34fc933f4369afd74e0e74b5de4
Md. Arafat Hossain <arafat07.ruet@gmail.com>
2022-07-23 13:00:48 +0600

    Initial Magic8Ball project

---
+++ Magic8Ball/Magic8Ball/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Magic8Ball
+//
+//  Created by Arafat Hossain on 23/7/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
