893a31fa64be81e9f03d514d013e64c0bfc01c25
Md. Arafat Hossain <arafat07.ruet@gmail.com>
2022-07-23 14:32:31 +0600

    [Xylophone] Added play logic

--- Xylophone/Xylophone/ViewController.swift
+++ Xylophone/Xylophone/ViewController.swift
@@ -7,17 +7,64 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var audioPlayer: AVAudioPlayer?
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-
-    
     @IBAction func notePressed(_ sender: UIButton) {
-        print(sender.tag)
+        var soundFileName : String = "C"
+        
+        switch sender.tag {
+        case 2:
+            soundFileName = "D"
+            break
+            
+        case 3:
+            soundFileName = "E"
+            break
+            
+        case 4:
+            soundFileName = "F"
+            break
+            
+        case 5:
+            soundFileName = "G"
+            break
+            
+        case 6:
+            soundFileName = "A"
+            break
+            
+        case 7:
+            soundFileName = "B"
+            break
+            
+        default:
+            soundFileName = "D"
+            break
+        }
+        
+        guard let xylophoneSound = Bundle.main.url(forResource: soundFileName, withExtension: "wav") else { return }
+
+            do {
+                try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+                try AVAudioSession.sharedInstance().setActive(true)
+
+                audioPlayer = try AVAudioPlayer(contentsOf: xylophoneSound, fileTypeHint: AVFileType.wav.rawValue)
+
+                guard let audioPlayer = audioPlayer else { return }
+
+                audioPlayer.play()
+
+            } catch let error {
+                print(error.localizedDescription)
+            }
     }
     
 }
