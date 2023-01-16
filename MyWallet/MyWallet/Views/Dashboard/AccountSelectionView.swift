451ef5df10da16c792a357977f537cf42fc1f3b4
Md. Arafat Hossain <arafat07.ruet@gmail.com>
2023-01-16 15:25:51 +0600

    [MyWallet] Add transaction view added

---
+++ MyWallet/MyWallet/Views/Dashboard/AccountSelectionView.swift
@@ -0,0 +1,20 @@
+//
+//  AccountSelectionView.swift
+//  MyWallet
+//
+//  Created by Arafat Hossain on 16/1/23.
+//
+
+import SwiftUI
+
+struct AccountSelectionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AccountSelectionView_Previews: PreviewProvider {
+    static var previews: some View {
+        AccountSelectionView()
+    }
+}
--- MyWallet/MyWallet/Views/Dashboard/AddTransactionView.swift
+++ MyWallet/MyWallet/Views/Dashboard/AddTransactionView.swift
@@ -10,11 +10,100 @@ import SwiftUI
 struct AddTransactionView: View {
     @Environment(\.dismiss) var dismiss
     
+    @State private var selectedType = AddType.expense
+    @State private var selectedAmount = 0.0
+    @State private var selectedDate = Date()
+    @State private var paymentType = PaymentType.cash
+    @State private var enteredNote = ""
+    @State private var enteredPayee = ""
+    
+    @FocusState private var isInputActive: Bool
+    
     var body: some View {
         NavigationView {
-            VStack {
-                Text("Add View")
-                    .font(.system(size: 20, weight: .bold))
+            Form {
+                Section {
+                    Picker("", selection: $selectedType) {
+                        ForEach(AddType.allCases, id: \.self) { type in
+                            Text(type.rawValue)
+                        }
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                    
+                    HStack {
+                        Text("\(UserDefaultHelper.shared.getCurrency().code)")
+                            .cornerRadius(10)
+                            .font(.system(size: 30, weight: .semibold))
+                        
+                        Spacer()
+                        
+                        TextField("0", value: $selectedAmount, format: .number)
+                            .lineLimit(1)
+                            .font(.system(size: 36, weight: .bold))
+                            .foregroundColor(self.getAmountTextColor())
+                            .multilineTextAlignment(.trailing)
+                            .keyboardType(.decimalPad)
+                            .focused($isInputActive)
+                    }
+                }
+                
+                Section("General") {
+                    HStack {
+                        Text("Account")
+                        Spacer()
+                        Text("Required")
+                            .foregroundColor(.red)
+                    }
+                    
+                    if selectedType == .transfer {
+                        HStack {
+                            Text("To Account")
+                            Spacer()
+                            Text("Required")
+                                .foregroundColor(.red)
+                        }
+                    }
+                    else {
+                        HStack {
+                            Text("Category")
+                            Spacer()
+                            Text("Required")
+                                .foregroundColor(.red)
+                        }
+                    }
+                    
+                    DatePicker("Date & Time", selection: $selectedDate)
+                }
+                .onTapGesture {
+                    isInputActive = false
+                }
+                
+                Section("More Detail") {
+                    NavigationLink(destination: AddDetail(text: $enteredNote, detailType: .note)) {
+                        HStack {
+                            Text("Note")
+                            Spacer()
+                            Text(enteredNote)
+                        }
+                    }
+                    
+                    NavigationLink(destination: AddDetail(text: $enteredPayee, detailType: .payee)) {
+                        HStack {
+                            Text("Payee")
+                            Spacer()
+                            Text(enteredPayee)
+                        }
+                    }
+                    
+                    Picker("Payment Type", selection: $paymentType) {
+                        ForEach(PaymentType.allCases, id: \.self) { payType in
+                            Text(payType.rawValue)
+                        }
+                    }
+                }
+                .onTapGesture {
+                    isInputActive = false
+                }
             }
             .toolbar {
                 ToolbarItemGroup(placement: .navigationBarLeading) {
@@ -32,10 +121,79 @@ struct AddTransactionView: View {
             }
         }
     }
+    
+    private func getAmountTextColor() -> Color {
+        switch selectedType {
+        case .expense:
+            return .red
+        case .income:
+            return .green
+        case .transfer:
+            return .primary
+        }
+    }
+}
+
+enum AddType: String, CaseIterable {
+    case expense = "Expense"
+    case income = "Income"
+    case transfer = "Transfer"
+}
+
+enum PaymentType: String, CaseIterable {
+    case cash = "Cash"
+    case debit = "Debit Card"
+    case transfer = "Bank Transfer"
+    case mobile = "Mobile Payment"
+    case credit = "Credit Card"
+    case other = "Other"
+}
+
+enum DetailType {
+    case note
+    case payee
+}
+
+struct AddDetail: View {
+    @Environment(\.dismiss) var dismiss
+    @Binding var text: String
+    @FocusState private var isInputActive: Bool
+    
+    var detailType: DetailType
+    
+    var body: some View {
+        VStack {
+            Text("\(self.getTitle())")
+            
+            TextField("", text: $text)
+                .multilineTextAlignment(.center)
+                .lineLimit(1)
+                .focused($isInputActive)
+                .onSubmit {
+                    Logger.i("Finished")
+                    dismiss()
+                }
+            
+            Spacer()
+        }
+        .onAppear {
+            isInputActive = true
+        }
+    }
+    
+    private func getTitle() -> String {
+        if detailType == .note {
+            return "Add Note"
+        }
+        else {
+            return "Add Payee"
+        }
+    }
 }
 
 struct AddTransactionView_Previews: PreviewProvider {
     static var previews: some View {
         AddTransactionView()
+//        AddDetail(text: .constant("Arafat"), detailType: .note)
     }
 }
